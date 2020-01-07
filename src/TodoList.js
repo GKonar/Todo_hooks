7d3c5a0fccf56d434b2b8ef96a7534e35b8af9cd
Grzegorz Konarski <gk@pubfront.com>
2020-01-07 10:10:51 +0100

    bump

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 
 function TodoList(props) {
