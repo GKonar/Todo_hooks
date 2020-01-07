cc96cdd9f496676189121a842ab0d3e4771c6173
Grzegorz Konarski <gk@pubfront.com>
2020-01-07 20:27:44 +0100

    Set key for each todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 
@@ -10,22 +10,16 @@ function TodoList({todos, removeTodo, toggleTodo, editTodo}) {
     return (
       <Paper>
         <List>
-          {todos.map((todo,i)  => (
-            <>
-              <Todo 
-                id={todo.id}
-                task={todo.task} 
-                key={todo.id} 
-                completed={todo.completed}
-                removeTodo={removeTodo}  
-                toggleTodo={toggleTodo}
-                editTodo={editTodo}
-              />
-              
-              { 
-                i < todos.length - 1 && <Divider /> 
-              }
-            </>
+          {todos.map(( todo, i ) => (
+              <Fragment key={todo.id}>
+                <Todo 
+                  {...todo}
+                  removeTodo={removeTodo}  
+                  toggleTodo={toggleTodo}
+                  editTodo={editTodo}
+                />
+                { i < todos.length - 1 && <Divider /> }
+              </Fragment>
           ))}
         </List>
       </Paper>
