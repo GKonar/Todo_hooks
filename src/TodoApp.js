d575caef9624df1d32e7683d440d7646759af81d
Grzegorz Konarski <gk@pubfront.com>
2020-01-07 10:10:42 +0100

    Create TodoForm, Add adding todo functionality

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,6 +6,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function TodoApp() {
   const initialTodos = [
@@ -14,6 +15,9 @@ function TodoApp() {
     { id: 3, task: "Grow mustache", coompleted: false },
   ];
   const [ todos, setTodos ] = useState(initialTodos)
+  const addTodo = newTodoText => {
+    setTodos([...todos, {id: 4, task: newTodoText, completed: false}]);
+  }
   return (
     <Paper
       style={{
@@ -29,6 +33,7 @@ function TodoApp() {
           <Typography color="inherit">Todos with hooks</Typography>
         </Toolbar>
       </AppBar>
+      <TodoForm addTodo={addTodo}/>
       <TodoList todos={todos} />
     </Paper>
   );
