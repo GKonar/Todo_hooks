6f91ee6910e59311a61e807bcc352caee18f1bb9
Grzegorz Konarski <gk@pubfront.com>
2020-01-07 20:26:50 +0100

    Creat useTodoState hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -12,34 +12,11 @@ import TodoForm from './TodoForm';
 
 function TodoApp() {
   const initialTodos = JSON.parse(window.localStorage.getItem('todos')) || "[]";
-  const [ todos, setTodos ] = useState(initialTodos);
+  const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(initialTodos); // extract all things this component need from hook 
 
   useEffect(() => {
     window.localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
-  
-  const addTodo = newTodoText => {
-    setTodos([...todos, {id: uuid(), task: newTodoText, completed: false}]);
-  }
-  
-  const removeTodo = todoId => {
-    const updatedTodos = todos.filter(todo => todo.id !== todoId);
-    setTodos(updatedTodos);
-  }
-
-  const toggleTodo = (todoId) => {
-    const updatedTodos = todos.map(todo => (
-      todo.id === todoId ? {...todo, completed: !todo.completed} : todo
-    ))
-    setTodos(updatedTodos);
-  }
-
-  const editTodo = (todoId, newTask) => {
-    const updatedTodos = todos.map(todo => (
-      todo.id === todoId ? {...todo, task: newTask} : todo
-    ))
-    setTodos(updatedTodos);
-  }
 
   return (
     <Paper
