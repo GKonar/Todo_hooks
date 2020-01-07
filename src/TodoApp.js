0c61863f0ae1f6f7754a669a0ad4819bac21d24b
Grzegorz Konarski <gk@pubfront.com>
2020-01-07 12:19:37 +0100

    Add remove, toggle, edit Todo

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -19,8 +19,28 @@ function TodoApp() {
   const [ todos, setTodos ] = useState(initialTodos)
   
   const addTodo = newTodoText => {
-    setTodos([...todos, {id: 4, task: newTodoText, completed: false}]);
+    setTodos([...todos, {id: uuid(), task: newTodoText, completed: false}]);
   }
+  
+  const removeTodo = todoId => {
+    const updatedTodos = todos.filter(todo => todo.id !== todoId);
+    setTodos(updatedTodos);
+  }
+
+  const toggleTodo = (todoId) => {
+    const updatedTodos = todos.map(todo => (
+      todo.id === todoId ? {...todo, completed: !todo.completed} : todo
+    ))
+    setTodos(updatedTodos);
+  }
+
+  const editTodo = (todoId, newTask) => {
+    const updatedTodos = todos.map(todo => (
+      todo.id === todoId ? {...todo, task: newTask} : todo
+    ))
+    setTodos(updatedTodos);
+  }
+
   return (
     <Paper
       style={{
@@ -38,8 +58,14 @@ function TodoApp() {
       </AppBar>
       <Grid container justify="center" style={{marginTop: "1rem"}}>
         <Grid item xs={11} md={8} lg={4}>
-          <TodoForm addTodo={addTodo} />
-          <TodoList todos={todos} />
+          <TodoForm 
+            addTodo={addTodo} />
+          <TodoList 
+            todos={todos} 
+            removeTodo={removeTodo}  
+            toggleTodo={toggleTodo}
+            editTodo={editTodo}
+          />
         </Grid>
       </Grid>
     </Paper>
